8a2a8e921e5c32b84779e8264147bb28d0fd197e
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-22 17:53:38 -0300

    removed async from ws server

--- rtm-text-share-backend/src/ws.ts
+++ rtm-text-share-backend/src/ws.ts
@@ -1,5 +1,5 @@
 const ws = (io: SocketIO.Server): void => {
-	io.on('connection', async (socket: any): Promise<void> => {
+	io.on('connection', (socket: any) => {
 		console.log(socket.id, 'has been connected from server');
 
 		socket.on('disconnect', () => console.log(socket.id, 'has been disconnected from server'));
