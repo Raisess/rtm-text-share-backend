68ab20340894a99d179d7c3aacc5d79fedb9c13a
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-23 10:57:48 -0300

    created create_user ws event && created arrays to store session and users && create getSessions and getUsers functions

--- rtm-text-share-backend/src/ws.ts
+++ rtm-text-share-backend/src/ws.ts
@@ -1,14 +1,23 @@
+import { IUser } from './interfaces/User';
 import { ISession } from './interfaces/Session';
 
+import { createUser } from './entities/user.entitie';
+
+let users:    Array<IUser>    = [];
 let sessions: Array<ISession> = [];
 
 export const ws = (io: SocketIO.Server): void => {
 	io.on('connection', (socket: any) => {
 		console.log(socket.id, 'has been connected to server');
 
+		socket.on('create_user', (userData: IUser) => {
+			users.push(createUser(socket.id, userData));
+		});
+
 		socket.on('disconnect', () => console.log(socket.id, 'has been disconnected from server'));
 	});
 }
 
 export const getSessions = (): Array<ISession> => sessions;
+export const getUsers    = (): Array<IUser>    => users;
 
