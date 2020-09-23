f182a443af5c7aa8d54e74a977e6ed5d83121a16
danillu <48914278+Raisess@users.noreply.github.com>
2020-09-23 10:07:43 -0300

    updated new user log

--- rtm-text-share-backend/src/ws.ts
+++ rtm-text-share-backend/src/ws.ts
@@ -1,6 +1,10 @@
+import { ISession } from './interfaces/Session';
+
+let sessions: Array<ISession> = [];
+
 const ws = (io: SocketIO.Server): void => {
 	io.on('connection', (socket: any) => {
-		console.log(socket.id, 'has been connected from server');
+		console.log(socket.id, 'has been connected to server');
 
 		socket.on('disconnect', () => console.log(socket.id, 'has been disconnected from server'));
 	});
